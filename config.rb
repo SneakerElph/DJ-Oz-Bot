f1ca4c600e8ec8cc20f1c0f3d13cd5850fc20367
Tyler <sneakerelph@gmail.com>
2016-04-24 15:12:04 -0700

    third commit

---
+++ config.rb
@@ -0,0 +1,17 @@
+#This config file sets up all of our stuff for PandoraBot!
+
+
+
+#Your Discord bot token. I think it needs quotes around it.
+ENV['discord_token'] = 'MTcxMzY0ODY2MzgzMTUxMTA2.CfWbtw.YtPwpGO0FYqyd0tJEqKrpw2kV6A'
+
+#Your discord application ID
+ENV['application_id'] = '171364652884819968'
+
+#What prefix does this bot respond to for commands?
+ENV['prefix'] = '!'
+
+#the voice channel ID you want PandoraBot to connect to TO DO: add instructions on how to get this number.
+ENV['current_voice_channel'] = '171156311231299585'
+
+ENV['pandorabot_name'] = 'Pandora'
