e4885f30c85130e278af1e45018ccb6e4a27ef0b
SneakerElph <tylerostby@gmail.com>
2016-04-24 15:13:03 -0700

    Update config.rb

--- config.rb
+++ config.rb
@@ -3,15 +3,15 @@
 
 
 #Your Discord bot token. I think it needs quotes around it.
-ENV['discord_token'] = 'MTcxMzY0ODY2MzgzMTUxMTA2.CfWbtw.YtPwpGO0FYqyd0tJEqKrpw2kV6A'
+ENV['discord_token'] = ''
 
 #Your discord application ID
-ENV['application_id'] = '171364652884819968'
+ENV['application_id'] = ''
 
 #What prefix does this bot respond to for commands?
 ENV['prefix'] = '!'
 
 #the voice channel ID you want PandoraBot to connect to TO DO: add instructions on how to get this number.
-ENV['current_voice_channel'] = '171156311231299585'
+ENV['current_voice_channel'] = ''
 
 ENV['pandorabot_name'] = 'Pandora'
