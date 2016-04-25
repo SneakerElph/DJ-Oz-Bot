45bcb01cacbf393840115c8d991bc46612fbf92b
Tyler <sneakerelph@gmail.com>
2016-04-24 17:06:27 -0700

    yeah

--- pandorabot.rb
+++ pandorabot.rb
@@ -3,36 +3,11 @@
 
 require 'discordrb'
 require './config.rb'
-$current_voice_channel = ENV['current_voice_channel'] #should move to config file
-$bot = Discordrb::Commands::CommandBot.new token: ENV['discord_token'], application_id: ENV['application_id'], prefix: ENV['prefix'] #should move all this to config file as well
-def play_song(event)
-	filelist = Dir.entries('.')
-#get those damn things out of here
-	filelist.delete(".")
-	filelist.delete("..")
-#let's figure out which file is newest in here, since that's the song that pianobarfly is likely currently playing
-	sorted_list = filelist.sort_by {|filename| File.ctime(filename)}
-#This list is backwards, sort the other way plz!
-	sorted_list.reverse!
-	file_to_play = sorted_list[0]
-#create the all-powerful VOICEBOT
-	voicebot = $bot.voice_connect($current_voice_channel)
-#show me the list because I don't trust you did it correctly
-	puts sorted_list
-#Set the "Now Playing" part of the bot to be the filename with any underscores replaced with spaces
-#Hopefully we don't play songs/artists with actual underscores in their names or SHIT WILL GET CRAZY INACCURATE YO
-	$bot.game=(file_to_play.gsub(".m4a" , "").gsub("_", " "))
-#tell the server we're doing something
-	event.respond "Hell yeah let's play some **#{file_to_play.gsub(".m4a", "").gsub("_", " ")}** up in here"
-#AW YEAH PLAY IT BABYYYYYYYYYYYYY
-	#event.voice.play_io(open(file_to_play))
-	event.voice.play_file(file_to_play)
-	#event.voice.play_stream(file_to_play)
-	event.voice.stop_playing
+require './functions.rb'
 
-#afterwards remove what's currently being played.
-	$bot.game=("Nothing.")
-end
+
+$current_voice_channel = ENV['current_voice_channel']
+$bot = Discordrb::Commands::CommandBot.new token: ENV['discord_token'], application_id: ENV['application_id'], prefix: ENV['prefix']
 
 $bot.command(:ping) do |event|
 	 username = event.user.name
@@ -74,7 +49,7 @@ end
 $bot.command(:pandora) do |event|
 	$songplaying = true
 	until $songplaying == false do
-			play_song(event)
+			play_newest_file(event)
 		end
 end
 
